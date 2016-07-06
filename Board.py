9c146a912705ea7064449da7ab717e0cb83d1f27
Matthew Ung <matthew.ung814@gmail.com>
2016-07-06 14:07:08 -0700

    working on board

--- Board.py
+++ Board.py
@@ -2,7 +2,14 @@ class Board:
     pieces = ['p','R','N','B','Q','K']
     initLoc = [['a','h',],['b','g'],\
                ['c','f'],['d'],['e']]
+    ep = list()
+    castleW, castleB = True, True
+    pLocW = dict()
+    plocB = dict()
+
     def __init__(self):
+        for piece in pieces:
+            pLoc[piece] = []
         self.reset()
 
     def reset(self):
@@ -18,7 +25,16 @@ class Board:
             self[chr(97+i)+'7'] = (Board.pieces[0],'black')
 
     def move(self, move):
-        
+        move = move.rstrip('+')
+        move = move.replace('x','')
+
+        if not move[0].isupper():
+            move = 'p'+move
+
+        if move[0] not in pieces:
+            return False
+
+        if validmove(    
 
     def __getitem__(self, key):
         x,y = list(key)
@@ -43,17 +59,75 @@ class Board:
                 out.append(piece[0]+' ')
             out.append('\n')
         return ''.join(out)
-
+    
 
 def validMove(piece, start, end):
-    x,y = ord(start[0])-97, int(start[1]) - 1
-    X,Y = ord(end[0])-97, int(end[1]) - 1
+    """determines if a move is valid"""
+    
+    #lower -> start
+    #upper -> end
+    y,x = ord(start[0])-97, int(start[1]) - 1
+    Y,X = ord(end[0])-97, int(end[1]) - 1
+
+    def fileCheck(x,y,X,Y):
+        if x == X:
+            #can't have pieces between start and end
+            for i in range(min(y,Y)+1,max(y,Y)):
+                if self.board[x][i][1] != 'None':
+                    return False
+            return True
+              
+        #same logic as above
+        elif y == Y:
+            for i inrange(min(x,X), max(x,X)):
+                if self.board[i][y][1] != 'None':
+                    return False
+            return True
+        return False
+    
+    def diagCheck(x,y,X,Y):
+        #positive slope
+        if x-X == y-Y:
+            for i in range(1, abs(X-x)):
+                if self.board[min(x,X)+i][min(y,Y)+i][1] != 'None':
+                    return False
+                return True
+        #negative slope
+        elif x+X == y+Y:
+            for i in range(1, abs(x-X)):
+                if self.board(min(x,X)+i][max(y,Y)-i][1] != 'None':
+                    return False
+            return True
+       return False
+    
+    #endcheck
+    if self.board[x][y][1] == self.board[X][Y][1]:
+        return False
+
+    #checking if values in range
     for i in [x,X,y,Y]:
         if i < 0 or i > 7:
             return False
 
+    #Rook
     if piece == 'R':
-        if x == X or y == Y:
+        return fileCheck(x,y,X,Y)
+
+    #Bishop
+    elif piece == 'B':
+        return diagCheck(x,y,X,Y)
+    
+    #Knight
+    elif piece == "N":
+        if abs(x-X)+abs(y-Y) == 3 and abs(x-X) > 0 and abs(x-X) < 0:
             return True
-    elif piece = 'B':
+    
+    #King
+    elif piece == 'K':
+        if abs(x-X) <= 1 and abs(y-Y) <= 1:
+            return True
+
+    #Queen
+    elif piece == 'Q':
+        return fileCheck(x,y,X,Y) or diagCheck(x,y,X,Y)
         
