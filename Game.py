a23a8d169bf440e0e9611de7d15417c20323757b
Matthew Ung <matthew.ung814@gmail.com>
2016-07-16 19:23:15 -0700

    added Game

---
+++ Game.py
@@ -0,0 +1,26 @@
+import Board
+
+
+class Game:
+    def __init__(self):
+        self.b = Board.Board()
+        
+    def printBoard(self):
+        print self.b
+    
+    def move(self, move):
+        self.b.move(move)
+    
+    
+
+if __name__ == "__main__":
+    g = Game()
+    try:
+        while True:
+            g.printBoard()
+            print "It is {} to move.".format(g.b.turn)
+            move = raw_input("Input a move: ")
+            g.move(move)             
+
+    except Exception as e:
+        print e
--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import Board
+import Game
 
-b = Board.Board()
+#b = Board.Board()
+g = Game.Game()
+print g.b
 
-
+"""
 b.move('f4')
 b.move('e5')
 b.move('g4')
 b.move('Qh4')
 
-print "checkCheck: " + str(b.checkCheck('white'))
-print "mateCheck: " + str(b.mateCheck('white'))
-
+"""
 """b.setUp('8/P7/8/3K4/5k2/8/5p2/8 w - - 0 1')
 
 b.move('a8Q')
